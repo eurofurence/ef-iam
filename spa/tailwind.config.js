b27a537d6e44ac67ab713e398912cb282162d238
Thiritin <thiritinthepone@gmail.com>
2020-02-23 22:59:45 +0100

    Added Tailwind Primary Colors

--- spa/tailwind.config.js
+++ spa/tailwind.config.js
@@ -1,11 +1,27 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#E6EFEE',
+          200: '#CBDEDD',
+          300: '#AEC6C4',
+          400: '#69A3A2',
+          500: '#005953',
+          600: '#00504B',
+          700: '#003532',
+          800: '#002825',
+          900: '#001B19'
+        }
+      }
+    }
+  },
   variants: {},
   plugins: []
 }
